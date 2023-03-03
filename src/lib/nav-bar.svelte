39f7769c24ec1f65607eb39fe858eb659f2767d7
justin-rowsell <112426519+justin-rowsell@users.noreply.github.com>
2023-03-03 15:06:48 -0300

    added link to source code

--- src/lib/nav-bar.svelte
+++ src/lib/nav-bar.svelte
@@ -8,16 +8,18 @@
         <img class="h-6 mr-3 sm:h-9" src="{logo}" alt="Easy as logo"/>
       </a>
     </div>
-    <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-main border-main hover:text-white hover:border-white">
-          <svg class="fill-main h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-        </button>
+    <div class="block sm:hidden">
+      <button class="flex items-center px-3 py-2 border rounded text-main border-main hover:text-white hover:border-white">
+        <svg class="fill-main h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <!-- <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            Docs
-          </a> -->
+    <div class="w-full block flex-grow md:flex md:items-center md:w-auto sm:hidden">
+        <div class="text-sm md:flex-grow">
+          <a href="https://github.com/justin-rowsell/portfolio" target="_blank" rel="noreferrer" 
+          class="block mt-4 sm:inline-block sm:mt-0 text-main hover:text-darkAccent mr-4 font-mono">
+            <span class="material-symbols-outlined align-bottom text-base">code</span> 
+            <span class="text-md">Source Code</span>
+          </a>
         </div>
     </div>
 </nav>
