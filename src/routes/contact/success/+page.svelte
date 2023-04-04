8acf2a841f79271e74d99720d04e40d5a3034d87
justin-rowsell <112426519+justin-rowsell@users.noreply.github.com>
2023-04-04 10:58:59 -0300

    send contact info complete

---
+++ src/routes/contact/success/+page.svelte
@@ -0,0 +1,17 @@
+<!--Basic html page with message indicating their submission was successful. 
+    Then the page will automatically redirect back to the home page-->
+<div class="ml-auto mr-auto text-center pt-16">
+    <h1>Thank you for your submission!</h1>
+    <p>You will be redirected back to the home page in 5 seconds.</p>
+</div>
+
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        setTimeout(() => {
+            goto(`/`, { replaceState: true });
+        }, 5000);
+    });
+</script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,9 @@ module.exports = {
         main: '#DC0000',
         darkAccent: '#850000',
         darkShade: '#141318',
+        danger: '#F44336',
+        success: '#777A38',
+        warning: '#F56A00'
       },
     },
   },
